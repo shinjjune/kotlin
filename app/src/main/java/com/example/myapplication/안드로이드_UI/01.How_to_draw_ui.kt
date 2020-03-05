e11b6cf7e60ede6d6dfb24318102def5852638bf
shinjs <sjh8236@naver.com>
2020-03-05 22:31:00 +0900

    android UI

--- app/src/main/java/com/example/myapplication/안드로이드_UI/01.How_to_draw_ui.kt
+++ app/src/main/java/com/example/myapplication/안드로이드_UI/01.How_to_draw_ui.kt
@@ -4,3 +4,19 @@ package com.example.myapplication.안드로이드_UI
 // - XML 을 이용한다.
 // - XML : DSL Language -> Domain Specific Language
 // - 안드로이드 UI 를 그리기 위해 특화된 언어이다.
+
+// 핸드폰마다 화면 크기가 다 다른데 어떻게 화면을 그려야 할까?
+// - 픽셀, dpi, dp 단위
+// - 픽셀: 핸드폰 화면에서 빛이 나오는 전구 : 가장 작은 단위
+// - dpi : dop per inch
+//      - ldpi : 120 (1인치에 120 픽셀)
+//      - mdpi : 160
+//      - hdpi : 240
+//      - xhdpi : 320
+//      - xxhdpi : 480
+//      - xxxhdpi: 640
+//  - dp -> Densitiy Independent Pixel -> 픽셀 독립적인 단위
+
+
+
+
