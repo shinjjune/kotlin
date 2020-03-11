510870e96e580dc0c2de241eb724c919c22da872
shinjs <sjh8236@naver.com>
2020-03-11 21:54:18 +0900

    Resource

---
+++ app/src/main/java/com/example/myapplication/Resource.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_fragment.*
+
+class Resource : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resource)
+
+        //1
+        val ment = resources.getString(R.string.hello)
+        Log.d("mentt","ment : " +ment)
+
+        //2
+        val ment2 = getString(R.string.hello)
+        Log.d("mentt","ment : " +ment2)
+
+
+        // SDK 버젼에 따른 분기 처리
+        val color = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            button.setBackgroundColor(getColor(R.color.textview_color))
+        } else {
+            button.setBackgroundColor(resources.getColor(R.color.textview_color))
+        }
+        // Log.d("mentt","color : " +color)
+    }
+}
