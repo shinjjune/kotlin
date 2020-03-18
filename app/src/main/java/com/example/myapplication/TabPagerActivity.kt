fc63c9e3c06c3a489d41ffd026baf52a55de44e5
shinjjune <sjh8236@naver.com>
2020-03-18 21:25:18 +0900

    tab & pager

---
+++ app/src/main/java/com/example/myapplication/TabPagerActivity.kt
@@ -0,0 +1,63 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TableLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_tab_pager.*
+
+class TabPagerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tab_pager)
+
+        tab_layout.addTab(tab_layout.newTab().setText("ONE"))
+        tab_layout.addTab(tab_layout.newTab().setText("TWO"))
+        tab_layout.addTab(tab_layout.newTab().setText("THREE"))
+
+        val pagerAdapter = PagerAdapter(supportFragmentManager, 3)
+        view_pager.adapter = pagerAdapter
+
+        tab_layout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                view_pager.currentItem = tab!!.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+
+            }
+        })
+    }
+}
+
+class PagerAdapter(
+    fragmentManager: FragmentManager,
+    val tabCount: Int
+):FragmentStatePagerAdapter(fragmentManager){
+    override fun getItem(position: Int): Fragment {
+        when(position) {
+            0->{
+                return Fragment1()
+            }
+            1->{
+                return Fragment2()
+            }
+            2->{
+                return Fragment3()
+            }
+            else -> return Fragment1()
+        }
+    }
+
+    override fun getCount(): Int {
+        return tabCount
+    }
+}
