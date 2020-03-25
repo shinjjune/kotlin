70a481123bf7642fbba24d304cbc6c41226f3a77
shinjjune <sjh8236@naver.com>
2020-03-25 22:23:48 +0900

    upload

--- app/src/main/java/com/example/myapplication/RetrofitService.kt
+++ app/src/main/java/com/example/myapplication/RetrofitService.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication
 
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -35,4 +37,11 @@ interface RetrofitService{
 
     @GET("/instagram/post/list/all")
     fun getAllPosts(): Call<ArrayList<Post>>
+
+    @Multipart
+    @POST("/instagram/post/")
+    fun uploadPost(
+        @Part image: MultipartBody.Part,
+        @Part ("content")requestBody: RequestBody
+    ):Call<Post>
 }
