9c15b705ecbdacc40290819bed42152ee96dd4d8
shinjjune <sjh8236@naver.com>
2020-04-02 21:17:09 +0900

    MELON

--- app/src/main/java/com/example/myapplication/YouTube.kt
+++ app/src/main/java/com/example/myapplication/YouTube.kt
@@ -5,6 +5,6 @@ import java.io.Serializable
 class YouTube (
     var title: String? =null,
     var content: String? =null,
-    var thumbnail: String? =null
+    var thumbnail: String? =null,
     var video: String? =null
 ):Serializable
